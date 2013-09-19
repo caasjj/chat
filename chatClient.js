0c6de0adecf16394d4401f5b9ece890efcc4197b
caasjj <caasjj@gmail.com>
2013-09-18 17:46:47 -0700

    Added a wherati change

--- chatClient.js
+++ chatClient.js
@@ -6,6 +6,8 @@
  * To change this template use File | Settings | File Templates.
  */
 
+var wherati = "Added by wherati";
+
 var chatterBox = (function (timerPeriod) {
 
     timerPeriod = timerPeriod || 3000;
